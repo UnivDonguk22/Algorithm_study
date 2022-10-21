faa8bab4347c06c6203595a39c318b8fbaef6a40
UnivDonguk22 <106023670+UnivDonguk22@users.noreply.github.com>
2022-10-22 03:52:28 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main(){
+    int a,b;
+    while(scanf("%d %d", &a, &b) == 2){
+        if(a == 0 && b == 0){
+            return 0;
+        }
+        printf("%d\n", (a+b));
+    }
+    
+    return 0;
+}
